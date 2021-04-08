1cd745a085f0576dac14936f47bb87eb043c376f
Matheus <math.reis.dev@gmail.com>
2021-04-08 13:16:45 -0300

    script reformulation

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-const catsName = document.getElementById("nameInput").value;
-const button = document.getElementById("btn");
+const input = document.getElementById("nameInput");
+const output = document.getElementById("resultOutput");
+const btn = document.getElementById("btn");
 
-const checkSyllables = () => {
-  for (i in catsName) {
-    if (catsName.match(/ee|i|y/gi)) {
-      document.getElementById("resultOutput").innerHTML =
-        "<b>Purrfect!</b><br>Your kitten's name is awesome and easily recognizable. For a better experience, remember to pronounce it with affection.";
-      return;
-    } else if (catsName.match(/a|e/gi)) {
-      document.getElementById("resultOutput").innerHTML =
-        "<b>Good!</b><br>Your kitten's name is great, and it is easy to recognize. Do not forget to reinforce the high-pitched syllables.";
-      return;
-    } else {
-      document.getElementById("resultOutput").innerHTML =
-        "<b>Not so good!</b><br>Your kitten's name is lovely, but it may be difficult to recognize. Don't worry, you should keep this name and follow some of the tips below.";
-      return;
-    }
-  }
-};
+btn.addEventListener("click", (event) => checkLetters());
+input.addEventListener("keyup", (event) => keyUp(event));
 
-checkSyllables(catsName);
-
-const keyUp = (e) => {
+function keyUp(event) {
   e = e || window.event;
   if (e.keyCode == 13) {
     document.getElementById("btn").click();
     return false;
   }
   return true;
-};
+}
+
+function checkLetters() {
+  const inputTxt = input.value;
+
+  if (/[a|b|c]+/gi.test(inputTxt)) {
+    output.innerText =
+      "Perfect! Your kitten's name is awesome and easily recognizable. For a better experience, remember to pronounce it with affection.";
+  } else if (/[d|e|f]+/gi.test(inputTxt)) {
+    output.innerText =
+      "Good! Your kitten's name is great, and it is easy to recognize. Do not forget to reinforce the high-pitched syllables.";
+  } else {
+    output.innerText =
+      "Not so good! Your kitten's name is lovely, but it may be difficult to recognize. Don't worry, you should keep this name and follow some of the tips below.";
+  }
+}
+
+checkLetters(inputTxT);
