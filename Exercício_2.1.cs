9b998e031650345642881b1670337ff792b23b20
Matheus Reis <math.reis.dev@gmail.com>
2021-07-10 13:16:18 -0700

    Add files via upload

---
+++ Exercício_2.1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercício_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Environment.UserName;
+
+            Console.WriteLine("Bem-vindo {0}, agora são {1:HH:mm:ss}.", name, DateTime.Now);
+
+            Console.WriteLine("Digite o 1º valor para a soma: ");
+            double firstValue = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2º valor para a soma: ");
+            double secondValue = Convert.ToDouble(Console.ReadLine());
+
+            double soma = firstValue + secondValue;
+
+            Console.WriteLine("{0}, o resultado da operação é {1}.", name, soma);
+        }
+    }
+}
