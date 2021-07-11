ca01045ae01796d00b946f58817c8a84513841e2
Matheus Reis <math.reis.dev@gmail.com>
2021-07-11 09:17:27 -0300

    Add files via upload

---
+++ Exercício 2.3/Enumerador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_2._3
+{
+    enum Operacao
+    {
+        Somar = 1,
+        Subtrair,
+        Multiplicar,
+        Dividir
+    }
+}
