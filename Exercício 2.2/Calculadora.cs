57a77e796acfc9a546e32bb77aec6f64b05263bc
Matheus Reis <math.reis.dev@gmail.com>
2021-07-15 09:42:04 -0300

    Add files via upload

---
+++ Exercício 2.2/Calculadora.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_2._2
+{
+    class Calculadora
+    {
+        // Atributos
+        private double valorA;
+        private double valorB;
+
+        // Construtor
+        public Calculadora(double valorA, double valorB)
+        {
+            this.valorA = valorA;
+            this.valorB = valorB;
+        }
+
+        // Métodos
+        public double Somar()
+        {
+            return valorA + valorB;
+        }
+        public double Subtrair()
+        {
+            return valorA - valorB;
+        }
+        public double Multiplicar()
+        {
+            return valorA * valorB;
+        }
+        public double Dividir()
+        {
+            return valorA / valorB;
+        }
+    }
+}
---
+++ Exercício 2.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercício_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor A: ");
+            double va = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o valor B: ");
+            double vb = Convert.ToDouble(Console.ReadLine());
+
+            Calculadora a = new Calculadora(va, vb);
+
+            Console.WriteLine("O resultado da soma de {0} e {1} é {2}.", va, vb, a.Somar());
+            Console.WriteLine("O resultado da substração de {0} e {1} é {2}.", va, vb, a.Subtrair());
+            Console.WriteLine("O resultado da multiplicação de {0} e {1} é {2}.", va, vb, a.Multiplicar());
+            Console.WriteLine("O resultado da divisão de {0} e {1} é {2}.", va, vb, a.Dividir());
+
+            Console.ReadLine();
+        }
+    }
+}
