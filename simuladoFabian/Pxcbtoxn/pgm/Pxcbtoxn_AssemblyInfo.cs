fa18c194ad58317d4d63144b3599c8e6336f36fa
Matheus Reis <math.reis.dev@gmail.com>
2021-07-27 06:40:26 -0300

    Add files via upload

---
+++ simuladoFabian/Pxcbtoxn/pgm/Pxcbtoxn_AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("Pxcbtoxn")]
+[assembly: AssemblyCompany("Banrisul")]
+[assembly: AssemblyProduct("Pxcbtoxn")]
+[assembly: AssemblyCopyright("Copyright © Banrisul 2013")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d50df0fc-5289-4d06-f42e-4145b601a07c")]
