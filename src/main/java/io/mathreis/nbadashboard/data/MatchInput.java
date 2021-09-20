e0bb305f5425a8fbf466c6646836f786cd9ff4c8
Matheus Reis <math.reis.dev@gmail.com>
2021-09-20 15:34:30 -0300

    getset

---
+++ src/main/java/io/mathreis/nbadashboard/data/MatchInput.java
@@ -0,0 +1,107 @@
+package io.mathreis.nbadashboard.data;
+
+public class MatchInput {
+
+    private String GAME_ID;
+    private String TEAM_NAME_HOME;
+    private String GAME_DATE;
+    private String WL_HOME;
+    private String PTS_HOME;
+    private String TEAM_NAME_AWAY;
+    private String WL_AWAY;
+    private String PTS_AWAY;
+    private String SEASON;
+    private String ATTENDANCE;
+    private String TEAM_CITY_NAME_HOME;
+    private String TEAM_NICKNAME_HOME;
+    private String TEAM_CITY_NAME_AWAY;
+    private String TEAM_NICKNAME_AWAY;
+    
+    public String getGAME_ID() {
+        return GAME_ID;
+    }
+    public void setGAME_ID(String gAME_ID) {
+        GAME_ID = gAME_ID;
+    }
+    public String getTEAM_NAME_HOME() {
+        return TEAM_NAME_HOME;
+    }
+    public void setTEAM_NAME_HOME(String tEAM_NAME_HOME) {
+        TEAM_NAME_HOME = tEAM_NAME_HOME;
+    }
+    public String getGAME_DATE() {
+        return GAME_DATE;
+    }
+    public void setGAME_DATE(String gAME_DATE) {
+        GAME_DATE = gAME_DATE;
+    }
+    public String getWL_HOME() {
+        return WL_HOME;
+    }
+    public void setWL_HOME(String wL_HOME) {
+        WL_HOME = wL_HOME;
+    }
+    public String getPTS_HOME() {
+        return PTS_HOME;
+    }
+    public void setPTS_HOME(String pTS_HOME) {
+        PTS_HOME = pTS_HOME;
+    }
+    public String getTEAM_NAME_AWAY() {
+        return TEAM_NAME_AWAY;
+    }
+    public void setTEAM_NAME_AWAY(String tEAM_NAME_AWAY) {
+        TEAM_NAME_AWAY = tEAM_NAME_AWAY;
+    }
+    public String getWL_AWAY() {
+        return WL_AWAY;
+    }
+    public void setWL_AWAY(String wL_AWAY) {
+        WL_AWAY = wL_AWAY;
+    }
+    public String getPTS_AWAY() {
+        return PTS_AWAY;
+    }
+    public void setPTS_AWAY(String pTS_AWAY) {
+        PTS_AWAY = pTS_AWAY;
+    }
+    public String getSEASON() {
+        return SEASON;
+    }
+    public void setSEASON(String sEASON) {
+        SEASON = sEASON;
+    }
+    public String getATTENDANCE() {
+        return ATTENDANCE;
+    }
+    public void setATTENDANCE(String aTTENDANCE) {
+        ATTENDANCE = aTTENDANCE;
+    }
+    public String getTEAM_CITY_NAME_HOME() {
+        return TEAM_CITY_NAME_HOME;
+    }
+    public void setTEAM_CITY_NAME_HOME(String tEAM_CITY_NAME_HOME) {
+        TEAM_CITY_NAME_HOME = tEAM_CITY_NAME_HOME;
+    }
+    public String getTEAM_NICKNAME_HOME() {
+        return TEAM_NICKNAME_HOME;
+    }
+    public void setTEAM_NICKNAME_HOME(String tEAM_NICKNAME_HOME) {
+        TEAM_NICKNAME_HOME = tEAM_NICKNAME_HOME;
+    }
+    public String getTEAM_CITY_NAME_AWAY() {
+        return TEAM_CITY_NAME_AWAY;
+    }
+    public void setTEAM_CITY_NAME_AWAY(String tEAM_CITY_NAME_AWAY) {
+        TEAM_CITY_NAME_AWAY = tEAM_CITY_NAME_AWAY;
+    }
+    public String getTEAM_NICKNAME_AWAY() {
+        return TEAM_NICKNAME_AWAY;
+    }
+    public void setTEAM_NICKNAME_AWAY(String tEAM_NICKNAME_AWAY) {
+        TEAM_NICKNAME_AWAY = tEAM_NICKNAME_AWAY;
+    }
+
+
+    
+}
