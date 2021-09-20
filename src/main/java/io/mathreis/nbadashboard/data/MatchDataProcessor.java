33cce083132f1c72aeccccbd4099118d651fdb6b
Matheus Reis <math.reis.dev@gmail.com>
2021-09-20 15:54:52 -0300

    getset

--- src/main/java/io/mathreis/nbadashboard/data/MatchDataProcessor.java
+++ src/main/java/io/mathreis/nbadashboard/data/MatchDataProcessor.java
@@ -1,23 +1,39 @@
 package io.mathreis.nbadashboard.data;
 
+import java.time.LocalDateTime;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.batch.item.ItemProcessor;
 
-public class MatchDataProcessor implements ItemProcessor<MatchInput, Person> {
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
 
-    private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
+    private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
 
     @Override
-    public Person process(final Person person) throws Exception {
-    final String firstName = person.getFirstName().toUpperCase();
-    final String lastName = person.getLastName().toUpperCase();
+    public Match process(final MatchInput matchInput) throws Exception {
 
-    final Person transformedPerson = new Person(firstName, lastName);
+        Match match = new Match();
+        match.setGameId(matchInput.getGameId());
+        match.setTeamNameHome(matchInput.getTeamNameHome());
+        match.setGameDat(LocalDateTime.parse(matchInput.getGameDate()));         
+        match.setWlHome(matchInput.getWlHome());
+        match.setPtsHome(Integer.parseInteger(matchInput.getPtsHome()));
+        match.setTeamNameAway(matchInput.getTeamNameAway());
+        match.setWlAway(matchInput.getWlAway());
+        match.setPtsAway(Integer.parseInteger(matchInput.getPtsAway()));
+        match.setSeason(matchInput.getSeason());
+        match.setAttendance(matchInput.getAttendance());
+        match.setTeamCityNameHome(matchInput.getTeamCityNameHome());
+        match.setTeamNicknameHome(matchInput.getTeamNicknameHome());
+        match.setTeamCityNameAway(matchInput.getTeamCityNameAway());
+        match.setTeamNicknameAway(matchInput.getTeamNicknameAway());
 
-    log.info("Converting (" + person + ") into (" + transformedPerson + ")");
 
-    return transformedPerson;
+
+
+
+        return match;
     }
 }
